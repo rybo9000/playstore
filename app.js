833d249d4cdf11db98c2283e1b41cb183ba6069f
Fielder <Ryan_Fielder@McAfee.com>
2020-01-08 20:58:04 -0600

    'final_work'

--- app.js
+++ app.js
@@ -4,15 +4,42 @@ const apps = require('./playstore.js');
 
 const app = express();
 
+app.use(morgan('dev'));
+
 app.get('/apps', (req, res) => {
     
-    const { sort } = req.query;
+    const { sort, genres } = req.query;
 
     if (sort) {
-        (sort === 'rating) || (sort === 'app') ? true : false;
-    }
+        if (!['rating', 'app'].includes(sort)) {
+          return res
+            .status(400)
+            .send('Sort must be one of rating or app');
+        }
+      }
 
-    res.json(apps);
+      if (genres) {
+        if (!['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].includes(genres)) {
+          return res
+            .status(400)
+            .send('Genres must be one of Action, Puzzle, Strategy, Casual, Arcade, or Card');
+        }
+      }
+
+      let results = apps;
+
+      if (genres) {
+        results = results.filter(result => result.Genres === genres);
+      }
+      
+      if (sort) {
+        results
+          .sort((a, b) => {
+            return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+        });
+      }
+
+      res.json(results);
 })
 
 app.listen(8000, () => {
